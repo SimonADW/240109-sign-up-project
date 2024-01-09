5a897b6f2c8b04e27e31420883c700ea711c9c6a
SimonADW <simon-winter@outlook.com>
2024-01-09 15:05:13 +0100

    Set autocomplete on inputs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("TEST");
+
+const submitButton = document.querySelector("button");
+const firstName = document.querySelector("#firstname");
+const lastName = document.querySelector("#lastname");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const password1 = document.querySelector("#password1");
+const password2 = document.querySelector("#password2");
+
+const passwordsMatch = ()=> {
+	return (password1.value === password2.value)
+}
+
+
+submitButton.addEventListener("click", ()=> {
+	console.log("no match");
+	
+});
